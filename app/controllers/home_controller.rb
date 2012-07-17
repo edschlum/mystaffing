2028c9969b90a8c29b68ecc47e6eb36a83c62621
Edouard Schlumberger <e.schlum@gmail.com>
2012-07-17 18:14:17 +0200

    applistaff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,6 @@
 class HomeController < ApplicationController
   skip_before_filter :require_login
   def index
-      @users = User.all
-      
-      
-      @nbr_dispos = Dispo.count(:statut_id, :conditions => ['statut_id = ?', '1'])
-      @nbr_staffe = Dispo.count(:statut_id, :conditions => ['statut_id = ?', '2'])
-      @nbr_malade = Dispo.count(:statut_id, :conditions => ['statut_id = ?', '3'])
-      @nbr_nesaitpas = Dispo.count(:statut_id, :conditions => ['statut_id = ?', '4'])
-      @total = Dispo.count(:statut_id)
       
       @lundi = Date.current().beginning_of_week(start_day = :monday) 
 
